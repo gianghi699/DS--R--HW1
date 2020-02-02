8744c46a4039fd087a46e44eacc2fe8c8ae26103
gianghi699 <55063173+gianghi699@users.noreply.github.com>
2020-02-02 15:29:49 -0500

    Add files via upload

---
+++ Esther_Le.R
@@ -0,0 +1,17 @@
+2017^(1/3)
+abs(5.7-6.8)/0.58
+a = 1:12
+a
+b = c(1,3,5,7,9,11)
+b
+c = seq(1,11,2)
+c
+ln.a = log(a)
+ln.a
+squares = b^2
+squares
+?sd
+#sd function computes the standard deviation of the values in x. If na.rm is true then missing values are removed before computation proceeds.
+Name = "Esther"
+paste("My name is", Name)
+
